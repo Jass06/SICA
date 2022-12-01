2d47ef8a2c2d541bdf56e28f108ac1f1262d9ac6
Jassyra <jespinozaq@goldenpalace.com.pe>
2022-12-01 16:59:24 -0500

    vHome3 Page Landing

--- js/main.js
+++ js/main.js
@@ -104,15 +104,81 @@ const sendEmail = (e) =>{
 contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+const scrollActive = () =>{
+    const scrollY = window.pageYOffset
 
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav-menu a[href*=' +sectionId+ ']')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    //Cuando el scroll esta a 350 , add the show-scroll
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
 
+//Previously selected topic
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+//Obtener la interface by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+//Validate if the user previously chose a topic
+if (selectedTheme){
+    //If the validation is fulfilled, we ask the issue was to know if we activated
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' :'remove'](iconTheme)
+}
+//Activate/deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    //Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    //We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    //scroll is greater than 50 viewport height, and the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+    //reset: true /* Animations repeat */
+})
+
+sr.reveal(`.home-data, .projects-container, .testimonial-container, .footer-container`)
+sr.reveal(`.home-info div`, {delay:600, origin: 'bottom', interval:100})
+sr.reveal(`.skills-content:nth-child(1), .contact-content:nth-child(1)`, { origin: 'left'})
+sr.reveal(`.skills-content:nth-child(2), .contact-content:nth-child(2)`, { origin: 'right'})
+sr.reveal(`.qualification-content, .services-card)`, { interval: 100})
